0ac3d6142e42a77ccf087b5046d4a886c3dd5d4d
Carlos Camargo <cicamargoba@unal.edu.co>
2024-08-07 15:33:07 -0500

    Adding lvgl source code

---
+++ littlevgl-8/lvgl/build/CMakeFiles/lvgl.dir/src/misc/lv_templ.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/lvgl.dir/src/misc/lv_templ.c.o: \
+ /Work/Proyects/PowerMeter/PowerWatchSoftware/Sitara/firmware/littlevgl-8/lvgl/src/misc/lv_templ.c \
+ /usr/aarch64-linux-gnu/include/stdc-predef.h
---
+++ littlevgl-8/lvgl/build/CMakeFiles/lvgl.dir/src/widgets/lv_objx_templ.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/lvgl.dir/src/widgets/lv_objx_templ.c.o: \
+ /Work/Proyects/PowerMeter/PowerWatchSoftware/Sitara/firmware/littlevgl-8/lvgl/src/widgets/lv_objx_templ.c \
+ /usr/aarch64-linux-gnu/include/stdc-predef.h
